2a1817977cb5df22ef673c366aaf7a4618efa45d
DLRDavid <ddelarosa0019@gmail.com>
2024-07-29 11:38:21 -0400

    made my changes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Add your code to this file
+
+
+
+let back = document.querySelector("body");
+back.style.background = "darkblue";
+
+let pokemonborder = document.querySelectorAll("img");
+for(let i= 0; i<pokemonborder.length;i++){
+    pokemonborder[i].style.border = "6px dotted orange";
+    console.log(pokemonborder);
+}
+
+let pokemonnames = document.querySelectorAll(".pokemon_names");
+for(let i= 0; i<pokemonnames.length;i++){
+    pokemonnames[i].style.color = "yellow";
+    console.log(pokemonnames);
+}
+
+
+
+//let pokemon_images = document.querySelectorAll("img");
+//pokemon_images.style.border = "8px dotted orange";
+//console.log(pokemon_images)
